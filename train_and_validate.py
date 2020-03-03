7bfa4699c3c974b0946efb6498b9933337a78000
Pandian <pandian-ra@users.noreply.github.com>
2020-03-03 01:55:30 -0800

    Update train_and_validate.py

--- train_and_validate.py
+++ train_and_validate.py
@@ -28,4 +28,49 @@ class train_and_validate()
             running_loss = 0.0
 
         print('Finished Training')
+  return model
 
+  def validate(testloader, device, model, labels)
+    dataiter = iter(testloader)
+    images, labels = dataiter.next()
+    images, labels = images.to(device), labels.to(device)
+    outputs = model(images)
+    correct = 0
+    total = 0
+    with torch.no_grad():
+      for data in testloader:
+        images, labels = data
+        images, labels = images.to(device), labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+  print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))    
+  
+  def classValidation()
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+      for data in testloader:
+        images, labels = data
+        images, labels = images.to(device), labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+
+    for i in range(10):
+      print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
+  
+  
+  
+  
+  
+  
+  
+  
