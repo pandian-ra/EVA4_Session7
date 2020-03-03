91c5a6bc5a1b2cd4a5739d4cd30b5a0356c35034
Pandian <pandian-ra@users.noreply.github.com>
2020-03-03 01:01:39 -0800

    Add files via upload

--- GetData.py
+++ GetData.py
@@ -7,32 +7,33 @@ import numpy as np
 
 
 
-def getData():
-    SEED = 1
-    # CUDA?
-    cuda = torch.cuda.is_available()
-    print("CUDA Available?", cuda)
-    # For reproducibility
-    torch.manual_seed(SEED)
-    
-    if cuda:
-        torch.cuda.manual_seed(SEED)
+class GetData():
+    def importDataset();
+        SEED = 1
+        # CUDA?
+        cuda = torch.cuda.is_available()
+        print("CUDA Available?", cuda)
+        # For reproducibility
+        torch.manual_seed(SEED)
+        
+        if cuda:
+            torch.cuda.manual_seed(SEED)
 
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        transform = transforms.Compose(
+            [transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
-    classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-    return (trainloader, testloader, classes)
+        trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                              shuffle=True, num_workers=2)
+        testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                             shuffle=False, num_workers=2)
+        classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+        return (trainloader, testloader, classes)
 #
 #
 #def imshow(img):
